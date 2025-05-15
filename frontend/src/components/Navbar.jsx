e2333c6bba3d6a36c47d51d18149c4c3fd89671d
meltingliquid <piyushraoc@gmail.com>
2025-05-16 02:21:25 +0530

    frontend also added
    frontend is added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAuthStore } from "../store/useAuthStore.js";
+
+
+const Navbar = () => {
+    const {authUser}=useAuthStore()
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useAuthStore=create((set)=>({
+    authUser:null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+    isCheckingAuth:true,
+    checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authUser: res.data });
+     
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+   signup: async (data) => {
+   set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+      get().connectSocket();
+    } catch (error) {
+        console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+
+   }
+}));
