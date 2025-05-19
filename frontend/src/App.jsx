4f3e9949bdc91d01f72402eff74a7d54b5cb1f7a
abrarullahhaqqani <abrarullahhaqqani@gmail.com>
2025-05-20 01:06:32 +0530

    Creating store for themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,64 @@
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAuthStore } from './store/useAuthStore.js';
-import { useEffect } from 'react';
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
+
 import { Loader } from "lucide-react";
-import { Toaster } from 'react-hot-toast';
-
-
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
-useEffect(() => {
-checkAuth();
-}, [checkAuth]);
+  console.log({ onlineUsers });
 
-console.log({ authUser });
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-if (isCheckingAuth && !authUser)
-return ( <div className="flex items-center justify-center h-screen"> <Loader className="size-10 animate-spin" /> </div>
-);
+  console.log({ authUser });
 
-return ( <div className="text-2xl font-bold text-blue-600"> <p>hello</p> <Navbar /> <Routes>
-<Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-      </Routes> 
-      
-
-      <Toaster/>
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
       </div>
-);
-};
+    );
 
+  return (
+    <div data-theme={theme}>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
 export default App;
