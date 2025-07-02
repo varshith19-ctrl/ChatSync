3a3436799ee4fc25cdcb77ce70dadff550751a13
varshith19-ctrl <varshithkaitoju@gmail.com>
2025-07-02 13:48:13 +0530

    Update package-lock.json for chatsync and set Node.js engine version; comment out messages display in ChatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -51,7 +51,7 @@ const ChatContainer = () => {
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
-      <p>messages</p>
+      {/* <p>messages</p> */}
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
           <div
